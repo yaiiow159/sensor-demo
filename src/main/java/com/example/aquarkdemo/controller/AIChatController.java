35cecedc9950fe9bc3099f1eced782458b63b8e3
Timmy <examyou076@gmail.com>
2024-08-16 03:31:59 +0800

    1. 更新AI-Chat聊天功能

--- src/main/java/com/example/aquarkdemo/controller/AIChatController.java
+++ src/main/java/com/example/aquarkdemo/controller/AIChatController.java
@@ -6,7 +6,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.ai.openai.OpenAiChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AIChatController {
 
-    private final OpenAiChatClient chatClient;
+    private final OllamaChatModel ollamaChatModel;
 
     @GetMapping("/")
     @Operation(summary = "進入AI小助手頁面", description = "進入AI小助手頁面", tags = "查詢")
@@ -29,11 +31,11 @@ public class AIChatController {
     @Operation(summary = "AI聊天", description = "根據前端請求訊息響應AI的訊息", tags = "AI-Chat")
     @ResponseBody
     public String chat(@Parameter(description = "訊息", example = "你好", required = true) @RequestBody String message) {
-        String response = "";
+        ChatResponse chatResponse;
         try {
-            response = chatClient.call(message);
-            log.debug("AI 回應訊息: {}", response);
-            return response;
+            chatResponse = ollamaChatModel.call(new Prompt(message));
+            log.debug("AI 回應訊息: {}", chatResponse.getResult().getOutput().getContent());
+            return chatResponse.getResult().getOutput().getContent();
         } catch (Exception e) {
             if(e.getMessage().contains("429")) return "已超過預定配額，請升級當前配額在進行操作";
             if(e.getMessage().contains("400")) return "請輸入正確的訊息";
@@ -41,7 +43,7 @@ public class AIChatController {
             if (e.getMessage().contains("500")) return "發生異常、請聯繫管理員";
             log.error("發生異常 原因: {}", e.getMessage());
         }
-        return response;
+        return "暫時無法回應您的問題";
     }
 
 }
